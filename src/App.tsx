cd25a01815eddbebaaeb81f778dbee823dcee9f3
Vladislav <vladozdeveloper@gmail.com>
2022-09-23 23:39:19 +0300

    created first block and style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const padding={padding:'10px'};
 
 function App() {
+  
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Container style={padding} maxWidth="md">
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        
+      </Grid>
+    </Container>
+    
   );
 }
 
