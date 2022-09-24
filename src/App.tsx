4127e0c3e5622a1eaaf7c1e1a8cb6805cdc8201e
Vladislav <vladozdeveloper@gmail.com>
2022-09-24 23:23:46 +0300

    created two div with currencyinput and currencytype, and typography with style

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { styled } from '@mui/material/styles';
 import MenuItem from '@mui/material/MenuItem';
 import TextField from '@mui/material/TextField';
 import Select from '@mui/material/Select';
+import { FormControl, InputLabel, Typography } from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,39 +19,72 @@ const Item = styled(Paper)(({ theme }) => ({
 const firstBlock={padding:'10px'};
 
 const cryptoInputBox={
-  display:'flex',
-  alignItems:'center',
-  justifyContent:"space-beetwen",
+  marginTop:'10px',
+};
+
+const currencyInput={
+  minWidth:'65%',
+  marginRight:'10px',
+};
+const currencyType={
+  minWidth:'32%',
 };
 
 function App() {
   
   return (
-    <Container style={firstBlock} maxWidth="md">
+    <Container style={firstBlock} maxWidth="lg">
       <Grid container spacing={2}>
         <Grid item xs={8}>
           <Item>xs=8</Item>
         </Grid>
         <Grid item xs={4}>
           <Item>
-            <div style={cryptoInputBox}>
-              <TextField
-                error
-                id="outlined-error"
-                label="Erro"
-                defaultValue="Hello"
-              />
+            <div >
+              <FormControl style={currencyInput}>
+                <TextField 
+                  fullWidth
+                  label="Сумма" 
+                  variant="outlined" />
+              </FormControl>
+              <FormControl style={currencyType}>
+                <InputLabel id="demo-simple-select-autowidth-label">Валюта</InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
-                  value={20}
-                  label="Age"
+                  value={10}
+                  label="Валюта"
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
+                  <MenuItem value={10}>USD</MenuItem>
                   <MenuItem value={20}>Twenty</MenuItem>
                   <MenuItem value={30}>Thirty</MenuItem>
                 </Select>
+              </FormControl>
             </div>
+            <div style={cryptoInputBox}>
+              <FormControl style={currencyInput}>
+                <TextField 
+                  fullWidth
+                  label="Сумма" 
+                  variant="outlined" />
+              </FormControl>
+              <FormControl style={currencyType}>
+                <InputLabel id="demo-simple-select-autowidth-label">Валюта</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={10}
+                  label="Валюта"
+                >
+                  <MenuItem value={10}>USD</MenuItem>
+                  <MenuItem value={20}>Twenty</MenuItem>
+                  <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <Typography mt={2} mb={-1} variant="h5" component="h5">
+              77,81 Российский рубль
+            </Typography>;
           </Item>
         </Grid>
         
