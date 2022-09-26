3a57f11e8e49f303fa1cde3279053215d104eff4
Vladislav <vladozdeveloper@gmail.com>
2022-09-26 23:16:34 +0300

    created types for interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-import { styled } from '@mui/material/styles';
 import CryptoTable from './components/CryptoTable';
 import ConverterBlock from './components/ConverterBlock';
+import { TCoin } from './types';
 
 const firstBlock={padding:'10px'};
 
 function App() {
+  const [allCoins,setAllCoins] = useState<TCoin[]>([]);
 
+  useEffect(()=>{
+    axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
+      .then((response)=>{
+        setAllCoins(response.data.Data.map((coin:any)=>{
+          const obj={
+            name:coin.CoinInfo.Name,
+            fullName:coin.CoinInfo.FullName,
+            imageUrl:`https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
+            price:coin.RAW.USD.PRICE.toFixed(3),
+            volume24Hour:coin.RAW.USD.VOLUME24HOUR.toFixed(0),
+          };
+          
+          return obj;
+        }));
+    })
+},[]);
   return (
     <Container style={firstBlock} maxWidth="lg">
       <Grid container spacing={2}>
         <Grid item xs={8}>
-          <CryptoTable/>
+          <CryptoTable items={allCoins}/>
         </Grid>
         <Grid item xs={4}>
           <ConverterBlock/>
         </Grid>
-        
       </Grid>
     </Container>
     
--- src/components/CryptoTable.tsx
+++ src/components/CryptoTable.tsx
@@ -1,47 +1,25 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { Paper } from "@mui/material";
-import axios from 'axios';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { TCoin } from "../types";
 
 const currencyIcon={
     width:'18px',
     height:'18px',
-  };
+};
 
-type TCoin={
-    name:string,
-    fullName:string,
-    imageUrl:string,
-    price:number,
-    volume24Hour:number,
+interface ICryptoTable{
+    items:TCoin[],
 }
 
-const CryptoTable=()=>{
+
+const CryptoTable: React.FC<ICryptoTable>=({items})=>{
     
-    const [allCoins,setAllCoins] = useState<TCoin[]>([]);
-
-    useEffect(()=>{
-        axios.get('https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD')
-          .then((response)=>{
-            setAllCoins(response.data.Data.map((coin:any)=>{
-              const obj={
-                name:coin.CoinInfo.Name,
-                fullName:coin.CoinInfo.FullName,
-                imageUrl:`https://www.cryptocompare.com/${coin.CoinInfo.ImageUrl}`,
-                price:coin.RAW.USD.PRICE.toFixed(3),
-                volume24Hour:coin.RAW.USD.VOLUME24HOUR.toFixed(0),
-              };
-              
-              return obj;
-            }));
-        })
-    },[]);
-
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
@@ -55,7 +33,7 @@ const CryptoTable=()=>{
                 </TableRow>
               </TableHead>
               <TableBody>
-                {!allCoins.length ? 'Загрузка...' : allCoins.map((coin) => (
+                {!items.length ? 'Загрузка...' : items.map((coin) => (
                   <TableRow
                     key={coin.name}
                     sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
